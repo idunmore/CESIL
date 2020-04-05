a7c76fdad0afbbda7a9480732548ca34d13ca7f9
idunmore <idunmore@users.noreply.github.com>
2020-04-04 17:24:10 -0700

    Interpreted implemented.

--- src/CESIL.py
+++ src/CESIL.py
@@ -1,50 +1,33 @@
 import enum
 
 def cesil():
-    print("CESIL (Computer Education in Schools Instruction Language")
-
+   
     interpreter = CESIL()
-    interpreter.load_file( r'./examples/Syntax_Test.ces')
-    pass
-
-class ElementType(enum.Enum):
-    Comment = 0
-    Label = 1
-    Instruction = 2
-    Operand = 3
-    InlineComment = 4
-
-class Element:
-
-    def __init__(self, value, type ):
-        self.value = value
-        self.type = type
-
-class Line:
-
-    def __init__(self, element1, element2, element3, element4):  
-        self.element1 = element1
-        self.element2 = element2
-        self.element3 = element3
-        self.element4 = element4
-
-    def getLine():
-        if element1.type == ElementType.Comment: return element1.value
-        line = ''
-
+    #interpreter.load_file( r'./examples/Syntax_Test.ces')
+    interpreter.load_file( r'./examples/Wikipedia.ces')
+    
+    interpreter.run()
 
 class OpType(enum.Enum):
     NoArg = 0
     Label = 1
     Literal = 2
     LiteralOrVar = 3
+    Var = 4
+
+class CodeLine():
+    def __init__(self, label, instruction, operand):
+        self.label = label
+        self.instruction = instruction
+        self.operand = operand
+        self.lineNumber = 0
     
 class CESIL:
 
     # Class Data
     instructions =  { 
         'IN': OpType.NoArg, 'OUT': OpType.NoArg, 'LINE': OpType.NoArg, 'HALT': OpType.NoArg,
-        'LOAD': OpType.LiteralOrVar, 'STORE': OpType.LiteralOrVar, 'ADD': OpType.LiteralOrVar,
+        'LOAD': OpType.LiteralOrVar, 'STORE': OpType.Var, 'ADD': OpType.LiteralOrVar,
         'SUBTRACT': OpType.LiteralOrVar, 'MULTIPLY': OpType.LiteralOrVar, "DIVIDE": OpType.LiteralOrVar,
         'PRINT': OpType.Literal, 'JUMP': OpType.Label, 'JIZERO': OpType.Label, 'JINEG': OpType.Label
     }
@@ -53,7 +36,6 @@ class CESIL:
 
     # Methods
     def __init__(self):
-        self._accumulator = 0
         self._program_lines = []
         self._data_values = []
         self._labels = {}
@@ -62,6 +44,7 @@ class CESIL:
     def load_file(self, filename):
         isCodeSection = True
         lineNumber = 0
+        instruction_index = 0
         with open( filename, 'r' ) as reader:
             for line in reader:
                 lineNumber = lineNumber + 1
@@ -75,8 +58,12 @@ class CESIL:
                         continue
                     else:
                         # Process Code Line
-                        self.parse_code_line(line)
-                    
+                        codeLine = self.parse_code_line(line)
+                        if codeLine.label != None: self._labels[codeLine.label] = instruction_index
+                        if codeLine.instruction != None:
+                            codeLine.lineNumber = lineNumber
+                            self._program_lines.append(codeLine)
+                        instruction_index = instruction_index + 1                    
                 else:
                     # We're in the Data Section, so add any data on this line to our data values.                    
                     if line[0] != '*': 
@@ -92,8 +79,6 @@ class CESIL:
         instruction = None
         operand = None
 
-        # TODO: For labels, resolve the instruction pointer (need to add an instruction pointer)
-        # and us it in place of the 0 (ZERO) we're currently inserting!
         if CESIL.is_legal_identifier(parts[currentPart]):
             # We have a label ...
             label = parts[currentPart]
@@ -106,10 +91,9 @@ class CESIL:
             opType = CESIL.instructions[instruction]
             
             # TODO: Streamline so that we abort with errors if we find them as we go,
-            # and otherwise only do the operand = potentialOperand assignement ONCE
+            # and otherwise only do the operand = potential Operand assignement ONCE
             # at the end of processing.
 
-            # TODO: 
             # Get Operand if there is one.
             if opType != OpType.NoArg :
                 if currentPart < lastPart: currentPart = currentPart + 1
@@ -121,7 +105,7 @@ class CESIL:
                     # Error
                     pass
 
-                if opType == OpType.LiteralOrVar:
+                if opType == OpType.LiteralOrVar or opType == OpType.Var:
                     if CESIL.is_legal_identifier(potentialOperand):
                         operand = potentialOperand
                         self._variables[operand] = 0
@@ -130,17 +114,81 @@ class CESIL:
                     else:   
                         #Error
                         pass
-
+                
+                # Only applies to PRINT, which needs special handling.
                 if opType == OpType.Literal:
-                    operand = potentialOperand
+                    # Put SPLIT parts back together for PRINT "" strings                    
+                    while currentPart < lastPart:
+                        if potentialOperand[-1] == '"': break
+                        currentPart = currentPart + 1
+                        potentialOperand = potentialOperand + ' ' + parts[currentPart]
 
-        print('Label: {0} - Instruction: {1} - Operand: {2}'.format(label, instruction, operand))
+                    # Strip Quotes
+                    operand = potentialOperand[1:-1]
 
+        return CodeLine(label, instruction, operand)
 
+    def run(self):
+        accumulator = 0
+        instructionPtr = 0
+        dataPtr = 0
+        while instructionPtr < len(self._program_lines):
+            line = self._program_lines[instructionPtr]
+            
+            # Execute the instruction, based on what it is.
+            if line.instruction == 'HALT': break
 
-    def what_is_part(part):
-        if CESIL.is_legal_identifier(): return 
+            if line.instruction == 'IN':
+                accumulator = int(self._data_values[dataPtr])
+                dataPtr = dataPtr + 1
+                
+            if line.instruction == 'OUT':
+                print(accumulator, end='')
+                
+            if line.instruction == 'LOAD':
+                if CESIL.is_legal_identifier(line.operand):
+                    accumulator = self._variables[line.operand]                                     
+                else:
+                    accumulator = int(line.operand)
+                           
+            if line.instruction == 'STORE':
+                if CESIL.is_legal_identifier(line.operand):
+                    self._variables[line.operand] = accumulator                    
 
+            if line.instruction == 'LINE': print('')
+
+            if line.instruction == 'PRINT': print(line.operand, end='')
+
+            if line.instruction == 'ADD':
+                accumulator = accumulator + int(self._variables[line.operand] if CESIL.is_legal_identifier(line.operand) else line.operand)
+
+            if line.instruction == 'SUBTRACT': 
+                accumulator = accumulator - int(self._variables[line.operand] if CESIL.is_legal_identifier(line.operand) else line.operand)
+
+            if line.instruction == 'MULTIPLY': 
+                accumulator = accumulator * int(self._variables[line.operand] if CESIL.is_legal_identifier(line.operand) else line.operand)
+
+            if line.instruction == 'DIVIDE': 
+                accumulator = accumulator + int(self._variables[lein.operand] if CESIL.is_legal_identifier(line.operand) else line.operand)
+
+            if line.instruction == 'JUMP':
+                instructionPtr = self._labels[line.operand]
+                continue
+
+            if line.instruction == 'JIZERO':
+                if accumulator == 0:
+                    instructionPtr = self._labels[line.operand]
+                    continue
+
+            if line.instruction == 'JINEG':
+                if accumulator < 0:
+                    instructionPtr = self._labels[line.operand]
+                    continue
+
+            # If nothing else has changed the execution path, move to the next instruction
+            instructionPtr = instructionPtr + 1
+
+   
     @staticmethod
     def is_legal_integer(value):
         try:
@@ -161,8 +209,11 @@ class CESIL:
     
     @staticmethod
     def is_legal_identifier(identifier):
-        # Identifiers must be between 1 and 6 characters, START with an alpha, contain only alpha-numerics and NOT be a reserved word/instruction.
-        return not CESIL.is_instruction(identifier) and (len(identifier) > 0 and len(identifier) <= 6) and (identifier[0].isalpha() and (len(identifier) == 1 or identifier[1:].isalnum()))
+        try:
+            # Identifiers must be between 1 and 6 characters, START with an alpha, contain only alpha-numerics and NOT be a reserved word/instruction.
+            return not CESIL.is_instruction(identifier) and (len(identifier) > 0 and len(identifier) <= 6) and (identifier[0].isalpha() and (len(identifier) == 1 or identifier[1:].isalnum()))
+        except:
+            return False
 
     @staticmethod
     def is_instruction(instruction):
