f6e146a30c7e2037b898b3a144c35a0b4735eadb
idunmore <idunmore@users.noreply.github.com>
2020-05-16 15:13:18 -0700

    Implement "PLUS" functionality; test files.

--- src/CESILPlus.py
+++ src/CESILPlus.py
@@ -81,27 +81,39 @@ def cesilplus(source, debug, plus, source_file):
     \b
       CESIL: Computer Eduction in Schools Instruction Language
 
-      "Plus" language extensions add a STACK to the language, enabled
-    with the -p | --plus options.  Extensions are DISABLED by default.
+      "Plus" language extensions add a STACK and SUBROUTINE support to
+    the language, enabled with the -p | --plus options.  Extensions are
+    DISABLED by default.
 
-      Extension instructions: PUSH, POP
+      "Plus" Mode - Extension instructions:
 
     \b
-        PUSH       - Pushes ACCUMULATOR value on to STACK
-        POP        - Pops top value from STACK into the ACCUMULATOR             
+        PUSH          - PUSHes the ACCUMULATOR value on to STACK
+        POP           - POPs top value from STACK into the ACCUMULATOR
+        
+        JUMPSR  label - Jumps to SUBROUTINE @ label
+        JSIZERO label - Jumps to SUBROUTINE @ label if ACCUMULATOR = 0
+        JSINEG  label - Jumps to SUBROUTINE @ label if ACCUMULATOR < 0
+        RETURN        - Returns from SUBROUTINE and continues execution
     """
     
     # Add "Plus" extension instructions, if "Plus" mode is selected
     if plus:
+        # Stack Instructions
         INSTRUCTIONS['POP'] = OpType.NONE
         INSTRUCTIONS['PUSH'] = OpType.NONE
+        # Subroutine Instructions
+        INSTRUCTIONS['RETURN'] = OpType.NONE
+        INSTRUCTIONS['JSINEG'] = OpType.LABEL
+        INSTRUCTIONS['JSIZERO'] = OpType.LABEL
+        INSTRUCTIONS['JUMPSR'] = OpType.LABEL
 
-    # Parsed program, variable and data storage - for execution
+    # Parsed program, variable and data storage - for execution ...
     program = load_program(source_file, source)    
-    
-    # Execute program ...
+    # ... and excute it ...
     run(program)
 
+# Loads program from a file, observing TEXT or PUNCH CARD formatting
 def load_program(filename, source_format):
     # Determine if we're parsing text file format or punch card
     is_text = True if source_format[0].upper() == 'T' else False
@@ -113,9 +125,10 @@ def load_program(filename, source_format):
 
     with open(filename, 'r') as reader:
         for line in reader:
-            lineNumber = lineNumber + 1
+            lineNumber += 1
             # Skip blank lines and comments.
-            if len(line) == 0 or line == '\n' or is_comment(line): continue
+            if len(line.strip()) == 0 or line == '\n' or is_comment(line):
+                continue
 
             if isCodeSection == True:           
                 # Transition from Code to Data?
@@ -123,18 +136,22 @@ def load_program(filename, source_format):
                     isCodeSection = False
                 else:
                     # Process Code Line
-                    codeLine = parse_code_line(program, line, is_text)
+                    codeLine = parse_code_line(line, is_text)
                     
-                    # Add an instruction pointer for a label
+                    # Add the label, if present, with its instruction pointer
                     if codeLine.label != None: 
                         program.labels[codeLine.label] = instruction_index
+
+                    # Add the variable, if present, initialized to zero
+                    if is_legal_identifier(codeLine.operand):
+                        program.variables[codeLine.operand] = 0
 
                     # Add a code line to the program if there's an instruction
                     if codeLine.instruction != None:
                         codeLine.lineNumber = lineNumber
                         program.program_lines.append(codeLine)
 
-                    instruction_index = instruction_index + 1                    
+                    instruction_index += 1                    
             else:
                 # We're in the Data Section, so add any data on this line to
                 # our data values.                    
@@ -144,17 +161,14 @@ def load_program(filename, source_format):
 
     return program
 
-# Parses a line of code using TEXT FILE formatting/layout rules
-def parse_code_line(program, line, is_text):
+# Parses a line of code, accounting for TEXT or PUNCH CARD formatting
+def parse_code_line(line, is_text):
 
+    # Break up the line, before figuring out what bits are where,
+    # using different splitting approaches for text vs. punch card.
     parts = None
-
-    # Break up the line, before figuring out what bits are where.
-    if is_text:
-        parts = line.split()
-    else:
-        parts = split_punch_card_line(line)
-
+    parts = line.split() if is_text else split_punch_card_line(line)
+    
     currentPart = 0
     lastPart = len(parts) -1
     label = None
@@ -164,9 +178,8 @@ def parse_code_line(program, line, is_text):
     if is_legal_identifier(parts[currentPart]):
         # We have a label ...
         label = parts[currentPart]
-        if currentPart < lastPart: currentPart = currentPart + 1
-        program.labels[label] = 0
-    
+        if currentPart < lastPart: currentPart +=  1
+           
     if is_instruction(parts[currentPart]):
         # We have an instruction
         instruction = parts[currentPart]
@@ -176,39 +189,41 @@ def parse_code_line(program, line, is_text):
         # go, and otherwise only do the operand = potential Operand
         # assignement ONCE at the end of processing.
 
-        # Get Operand if there is one.
+        # Get the Operand if there is one.
         if opType != OpType.NONE:
-            if currentPart < lastPart: currentPart = currentPart + 1
+            if currentPart < lastPart: currentPart += 1
             potentialOperand = parts[currentPart]
             # Validate the potential operand
-            if opType == OpType.LABEL and is_legal_identifier(potentialOperand):
-                operand = potentialOperand
+            if ( opType == OpType.LABEL
+                 and is_legal_identifier(potentialOperand) ):
+                    operand = potentialOperand
             else:
                 # Error
                 pass
 
             if opType == OpType.LITERAL_VAR or opType == OpType.VAR:
                 if is_legal_identifier(potentialOperand):
-                    operand = potentialOperand
-                    program.variables[operand] = 0
+                    operand = potentialOperand                    
                 elif is_legal_integer(potentialOperand):
                     operand = int(potentialOperand)
                 else:   
-                    #Error
+                    # Error
                     pass
             
             # Only applies to PRINT, which needs special handling.
             if opType == OpType.LITERAL:
                 # Put SPLIT parts back together for PRINT "" strings                    
                 while currentPart < lastPart:
-                    currentPart = currentPart + 1
-                    potentialOperand = potentialOperand + ' ' + parts[currentPart]
+                    currentPart += 1
+                    potentialOperand += ' ' + parts[currentPart]
 
                 # Strip Quotes and any trailing comment.
-                operand = potentialOperand[potentialOperand.find('"')+1:potentialOperand.rfind('"')]
+                operand = potentialOperand[potentialOperand.find('"')+1:
+                            potentialOperand.rfind('"')]
 
     return CodeLine(label, instruction, operand)
 
+# Splits up source-code lines based on PUNCH CARD column settings.
 def split_punch_card_line(line):
     parts = []
     length = len(line)
@@ -240,22 +255,31 @@ def split_punch_card_line(line):
         
     return parts
 
-
+# Executes the specified, parsed, CESIL program.
 def run(program):
+    # "Pure" CESIL execution state
     accumulator = 0
-    stack = []
-    instructionPtr = 0
+    instruction_ptr = 0
     dataPtr = 0
-    while instructionPtr < len(program.program_lines):
-        line = program.program_lines[instructionPtr]
-        variables = program.variables
 
+    # "Plus" execution state
+    stack = []
+    call_stack = []
+
+    # Shorter references for frequently accessed program elements
+    variables = program.variables
+    labels = program.labels
+
+    # Iterate the "program", 
+    while instruction_ptr < len(program.program_lines):
+        line = program.program_lines[instruction_ptr]
+        
         # Execute the instruction, based on what it is.
         if line.instruction == 'HALT': break
 
         if line.instruction == 'IN':
             accumulator = int(program.data_values[dataPtr])
-            dataPtr = dataPtr + 1
+            dataPtr += 1
         elif line.instruction == 'OUT':
             print(accumulator, end='')
         elif line.instruction == 'LOAD':
@@ -268,40 +292,58 @@ def run(program):
         elif line.instruction == 'PRINT':
             print(line.operand, end='')
         elif line.instruction == 'ADD':
-            accumulator = accumulator + get_real_value(variables, line.operand)
+            accumulator += get_real_value(variables, line.operand)
         elif line.instruction == 'SUBTRACT': 
-            accumulator = accumulator - get_real_value(variables, line.operand)
+            accumulator -= get_real_value(variables, line.operand)
         elif line.instruction == 'MULTIPLY': 
-            accumulator = accumulator * get_real_value(variables, line.operand)
+            accumulator *= get_real_value(variables, line.operand)
         elif line.instruction == 'DIVIDE': 
-            accumulator = accumulator / get_real_value(variables, line.operand)
+            accumulator /= get_real_value(variables, line.operand)
         elif line.instruction == 'JUMP':
-            instructionPtr = program.labels[line.operand]
+            instruction_ptr = labels[line.operand]
             continue
         elif line.instruction == 'JIZERO':
             if accumulator == 0:
-                instructionPtr = program.labels[line.operand]
+                instruction_ptr = labels[line.operand]
                 continue
         elif line.instruction == 'JINEG':
             if accumulator < 0:
-                instructionPtr = program.labels[line.operand]
-                continue       
-        elif line.instruction == 'POP':
-            accumulator = stack.pop
+                instruction_ptr = labels[line.operand]
+                continue
+        elif line.instruction == 'RETURN':
+            instruction_ptr = call_stack.pop()
+        elif line.instruction == 'JUMPSR':
+            call_stack.append(instruction_ptr)
+            instruction_ptr = labels[line.operand]
             continue
+        elif line.instruction == 'JSIZERO':
+            if accumulator == 0:
+                call_stack.append(instruction_ptr)
+                instruction_ptr = labels[line.operand]
+                continue
+        elif line.instruction == 'JSINEG':
+            if accumulator < 0:
+                call_stack.append(instruction_ptr)
+                instruction_ptr = labels[line.operand]
+                continue
+        elif line.instruction == 'POP':
+            accumulator = stack.pop()
         elif line.instruction == 'PUSH':
             stack.append(accumulator)
-            continue
 
-        # If nothing else has changed the execution path, move to the next instruction
-        instructionPtr = instructionPtr + 1
+        # If nothing else has changed the execution path, move to the
+        # next instruction
+        instruction_ptr += 1
 
+# Resolves Operand value as a LITERAL or from a VARIABLE, and returns it.
 def get_real_value(variables, operand):
     return int(variables[operand] if is_legal_identifier(operand) else operand)
 
+# Determines if the line is a comment.
 def is_comment(line):
     return len(line) > 0 and line[0] in COMMENT_PREFIX
 
+# Determines if the IDENTIFIER is legal in CESIL
 def is_legal_identifier(identifier):
     if is_instruction(identifier):
         # Instructions are RESERVED words and NOT legal identifiers!
@@ -309,6 +351,7 @@ def is_legal_identifier(identifier):
     else:
         return re.fullmatch(IDENTIFIER_PATTERN, str(identifier)) is not None
 
+# Bounds checks a value as a legal INTEGER (24-bit, signed).
 def is_legal_integer(value):
     try:
         num = int(value)
@@ -316,9 +359,11 @@ def is_legal_integer(value):
     except:
         return False
 
+# Determine if arugment is a valid CESIL (or CESIL Plus) instruction.
 def is_instruction(instruction):
     return instruction in INSTRUCTIONS
 
+# Determine if this line indicates the start of the "Data Section"
 def is_data_start(line):
     return len(line) > 0 and line[0] == START_DATA_SECTION
 
