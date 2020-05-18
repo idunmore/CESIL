7913bafa48c5da68b8ca6051cccac0ec78c0340b
idunmore <idunmore@users.noreply.github.com>
2020-05-17 17:09:53 -0700

    Added basic DEBUG funcs, exceptions, bug fixes.

--- src/CESILPlus.py
+++ src/CESILPlus.py
@@ -36,6 +36,29 @@ class Program():
         self.labels = {}
         self.variables = {}
 
+# State - Represents the current execution state of the Program
+class State():
+    def __init__(self):
+        # Pure CESIL state
+        self.accumulator = 0
+        self.instruction_ptr = 0
+        self.data_ptr = 0
+        # "Plus" execution state
+        self.stack = []
+        self.call_stack = []
+
+# Exceptions
+
+class CESILException(Exception):
+    def __init__(self, line_number, message, code):
+        self.line_number = line_number
+        self.message = message
+        self.code = code
+    
+    def print(self):
+        print('Error {0} at line {1}: {2}',
+            self.message, self.line_number, self.code)
+
 # Constants
 
 # CESIL identifiers/labels consist of up to 6 uppercase alphanumeric
@@ -69,7 +92,7 @@ INSTRUCTIONS = {
     type=click.Choice(['t', 'text', 'c', 'card'], case_sensitive=False),
     default='text', show_default=True, help='Text or punched Card input.')
 @click.option('-d', '--debug',
-    type=click.Choice(['0', '1', '2'], case_sensitive=False),
+    type=click.Choice(['0', '1', '2', '3', '4'], case_sensitive=False),
         default='0', show_default=True, help='Debug mode/verbosity level.')
 @click.option('-p', '--plus', is_flag=True, default=False,
     help='Enables "plus" mode language extensions.')
@@ -108,11 +131,15 @@ def cesilplus(source, debug, plus, source_file):
         INSTRUCTIONS['JSIZERO'] = OpType.LABEL
         INSTRUCTIONS['JUMPSR'] = OpType.LABEL
 
-    # Parsed program, variable and data storage - for execution ...
-    program = load_program(source_file, source)    
-    # ... and excute it ...
-    run(program)
+    try:
+        # Parsed program, variable and data storage - for execution ...
+        program = load_program(source_file, source)    
+        # ... and excute it ...
+        run(program, int(debug))
 
+    except CESILException as err:
+        err.print()
+    
 # Loads program from a file, observing TEXT or PUNCH CARD formatting
 def load_program(filename, source_format):
     # Determine if we're parsing text file format or punch card
@@ -142,9 +169,12 @@ def load_program(filename, source_format):
                     if code_line.label != None: 
                         program.labels[code_line.label] = instruction_index
 
-                    # Add the variable, if present, initialized to zero
-                    if is_legal_identifier(code_line.operand):
-                        program.variables[code_line.operand] = 0
+                    # Variable? (A legal IDENTIFIER that ISN'T a LABEL)
+                    if ( is_legal_identifier(code_line.operand) and                       
+                         INSTRUCTIONS[code_line.instruction] ==
+                         OpType.LITERAL_VAR):
+                            # Add the variable and initialize it                          
+                            program.variables[code_line.operand] = 0
 
                     # Add a code line to the program if there's an instruction
                     if code_line.instruction != None:
@@ -197,8 +227,8 @@ def parse_code_line(line, is_text):
             if ( op_type == OpType.LABEL
                  and is_legal_identifier(potential_operand) ):
                     operand = potential_operand
-            else:
-                # Error
+            else:          
+                # Error - Illegal Idenfier?      
                 pass
 
             if op_type == OpType.LITERAL_VAR or op_type == OpType.VAR:
@@ -207,7 +237,7 @@ def parse_code_line(line, is_text):
                 elif is_legal_integer(potential_operand):
                     operand = int(potential_operand)
                 else:   
-                    # Error
+                    # Error - Illegal operand
                     pass
             
             # Only applies to PRINT, which needs special handling.
@@ -256,84 +286,82 @@ def split_punch_card_line(line):
     return parts
 
 # Executes the specified, parsed, CESIL program.
-def run(program):
-    # "Pure" CESIL execution state
-    accumulator = 0
-    instruction_ptr = 0
-    data_ptr = 0
-
-    # "Plus" execution state
-    stack = []
-    call_stack = []
+def run(program, debug_level):
+   # Initialize execution state
+    state = State()
 
     # Shorter references for frequently accessed program elements
     variables = program.variables
     labels = program.labels
 
     # Iterate the "program", 
-    while instruction_ptr < len(program.program_lines):
-        line = program.program_lines[instruction_ptr]
+    while state.instruction_ptr < len(program.program_lines):
+        # Output debug info, if enabled - for line ABOUT to execute!
+        if debug_level > 0: debug_out( debug_level, program, state)
+
+        # Get line to execute.
+        line = program.program_lines[state.instruction_ptr]
         
         # Execute the instruction, based on what it is.
         if line.instruction == 'HALT': break
 
         if line.instruction == 'IN':
-            accumulator = int(program.data_values[data_ptr])
-            data_ptr += 1
+            state.accumulator = int(program.data_values[data_ptr])
+            state.data_ptr += 1
         elif line.instruction == 'OUT':
-            print(accumulator, end='')
+            print(state.accumulator, end='')
         elif line.instruction == 'LOAD':
-            accumulator = get_real_value(variables, line.operand)
+            state.accumulator = get_real_value(variables, line.operand)
         elif line.instruction == 'STORE':
             if is_legal_identifier(line.operand):
-                variables[line.operand] = accumulator                    
+                variables[line.operand] = state.accumulator                    
         elif line.instruction == 'LINE':
             print('')
         elif line.instruction == 'PRINT':
             print(line.operand, end='')
         elif line.instruction == 'ADD':
-            accumulator += get_real_value(variables, line.operand)
+            state.accumulator += get_real_value(variables, line.operand)
         elif line.instruction == 'SUBTRACT': 
-            accumulator -= get_real_value(variables, line.operand)
+            state.accumulator -= get_real_value(variables, line.operand)
         elif line.instruction == 'MULTIPLY': 
-            accumulator *= get_real_value(variables, line.operand)
+            state.accumulator *= get_real_value(variables, line.operand)
         elif line.instruction == 'DIVIDE': 
-            accumulator /= get_real_value(variables, line.operand)
+            state.accumulator /= get_real_value(variables, line.operand)
         elif line.instruction == 'JUMP':
-            instruction_ptr = labels[line.operand]
+            state.instruction_ptr = labels[line.operand]
             continue
         elif line.instruction == 'JIZERO':
-            if accumulator == 0:
-                instruction_ptr = labels[line.operand]
+            if state.accumulator == 0:
+                state.instruction_ptr = labels[line.operand]
                 continue
         elif line.instruction == 'JINEG':
-            if accumulator < 0:
-                instruction_ptr = labels[line.operand]
+            if state.accumulator < 0:
+                state.instruction_ptr = labels[line.operand]
                 continue
         elif line.instruction == 'RETURN':
-            instruction_ptr = call_stack.pop()
+            state.instruction_ptr = state.call_stack.pop()
         elif line.instruction == 'JUMPSR':
-            call_stack.append(instruction_ptr)
-            instruction_ptr = labels[line.operand]
+            state.call_stack.append(state.instruction_ptr)
+            state.instruction_ptr = labels[line.operand]
             continue
         elif line.instruction == 'JSIZERO':
-            if accumulator == 0:
-                call_stack.append(instruction_ptr)
-                instruction_ptr = labels[line.operand]
+            if state.accumulator == 0:
+                state.call_stack.append(state.instruction_ptr)
+                state.instruction_ptr = labels[line.operand]
                 continue
         elif line.instruction == 'JSINEG':
-            if accumulator < 0:
-                call_stack.append(instruction_ptr)
-                instruction_ptr = labels[line.operand]
+            if state.accumulator < 0:
+                state.call_stack.append(state.instruction_ptr)
+                state.instruction_ptr = labels[line.operand]
                 continue
         elif line.instruction == 'POP':
-            accumulator = stack.pop()
+            state.accumulator = state.stack.pop()
         elif line.instruction == 'PUSH':
-            stack.append(accumulator)
+            state.stack.append(state.accumulator)
 
         # If nothing else has changed the execution path, move to the
         # next instruction
-        instruction_ptr += 1
+        state.instruction_ptr += 1
 
 # Resolves Operand value as a LITERAL or from a VARIABLE, and returns it.
 def get_real_value(variables, operand):
@@ -367,6 +395,77 @@ def is_instruction(instruction):
 def is_data_start(line):
     return len(line) > 0 and line[0] == START_DATA_SECTION
 
+# Debug Output
+def debug_out(level, program, state):
+
+    # Summary Output
+    if level == 1 or level == 2:
+        # Code Line, accumulator value, top stack value
+        line = program.program_lines[state.instruction_ptr]
+        label = str(line.label if line.label is not None else '')
+        if line.operand is None:
+            operand = ''
+        elif line.instruction == 'PRINT':
+            operand = '"{0}"'.format(line.operand)
+        else:
+            operand = line.operand
+
+        if len(state.stack) > 0:
+            top_of_stack = str(state.stack[len(state.stack)-1])
+        else:
+            top_of_stack = 'Empty'
+
+        ## Accumulator State Flag (ZERO, NEG or none)
+        flags = ''
+        if state.accumulator == 0:
+            flags = 'ZERO'
+        elif state.accumulator < 0:
+            flags = 'NEG'
+    
+        summary_format = ' [Accumlator: {0:>10}] [Flags: {1:>4}]'
+        summary_format += ' [Stack Top: {2:>10s}]'
+        summary_format += ' -> {3:<8}{4:<8} {5}'
+        print('\nDEBUG:')
+        print(summary_format.format(state.accumulator, flags, top_of_stack,
+              label, line.instruction, operand))
+               
+    # Verbose output
+    if level == 3 or level == 4:
+        ouput_stack_variable_detail(program, state)
+
+    if level == 2 or level == 4:
+        input('Press [Enter] to EXECUTE this line.\n')
+        pass
+
+# Outputs details for STACK and VARIABLE values.
+def ouput_stack_variable_detail(program, state):         
+    print(' STACK (Top-Down)                   VARIABLES')
+    print(' (Top-Down)                Name   | Value   ')
+
+    # Which list has more items in it, the stack or the variable list?
+    longest_list = max(len(state.stack), len(program.variables))
+    index = 0
+    while index < longest_list:
+        stack_item = ''
+        variable_name = ''
+        variable_value = ''
+
+        # Do current stack item, if there is one.
+        if index < len(state.stack):
+            stack_item = state.stack[(len(state.stack) - index) - 1]
+       
+        # Do next variable, if there is one.
+        if index < len(program.variables):
+            variable_name = list(program.variables)[index - 1]
+            variable_value = program.variables[variable_name]
+       
+        line_fmt = '  {0:>8}'
+        if variable_name != '': line_fmt += '                 {1:<6} = {2:>8}'
+        
+        print(line_fmt.format( stack_item, variable_name, variable_value))
+
+        index += 1            
+    
 # Run!
 if __name__ == '__main__':
     cesilplus() 
